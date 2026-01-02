ff850bb415c71ae7db2df07f5df22a3f687ca437
Usethatname <87234407+Usethatname@users.noreply.github.com>
2026-01-02 16:11:39 +0700

    working key buttons hell yah

--- CPlugin/Class1.cs
+++ CPlugin/Class1.cs
@@ -18,56 +18,27 @@ namespace CPlugin
 
         private readonly Harmony harmony = new Harmony(id);
 
-        public static bool IsLagEnabled = false;
-
         public override void Load()
         {
             Log.LogInfo($"Плагин {plugin_name} загружен!");
 
             harmony.PatchAll();
-            
-        public override void Unload()
-        {
-            harmony.UnpatchSelf();
-            Log.LogInfo($"Плагин {plugin_name} выгружен.");
-            base.Unload();
         }
-    }
-
-    [HarmonyPatch]
-    internal static class Patches
+// реализация пачта на нажатие клавиши
+    [HarmonyPatch(typeof(PlayerControl), "FixedUpdate")]
+    public static class PlayerControl_FixedUpdatePatch
     {
-        [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.RpcSendChat))]
-        [HarmonyPrefix]
-        private static bool RpcSendChat_Prefix(PlayerControl __instance, string chatText)
+        public static void Postfix(PlayerControl __instance)
         {
-            if (!Plugin.IsLagEnabled)
+            PlayerControl local = PlayerControl.LocalPlayer;
+            if (local == null) return;
+
+            if (Input.GetKey(KeyCode.F1))
             {
-                return true;
+
+                PlayerControl.LocalPlayer.RpcSetName("sosun");
+                
             }
-
-            // Для логирования из статического класса нужно получить доступ к логгеру плагина.
-            // Один из способов - передать его в конструктор, но проще сделать его публичным статическим.
-            // В данном примере мы оставим логирование в основном классе плагина.
-            // Если нужно здесь, можно сделать Log в Plugin публичным статическим.
-            // Plugin.Log.LogWarning("Перехват отправки чата..."); // <-- так сработает, если Log в Plugin публичный
-
-            MessageWriter writer = AmongUsClient.Instance.StartRpc(__instance.NetId, (byte)RpcCalls.SendChat);
-            int maliciousStringLength = 999999;
-            writer.Write(maliciousStringLength);
-            writer.EndMessage();
-            
-            return false;
         }
     }
-    
-void Update(){
-
-    if (Input.GetKeyDown(KeyCode.L))
-    {
-        IsLagEnabled = !IsLagEnabled;
-    }
-}
-
-}
 }
